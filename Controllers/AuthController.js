03ce6724038dfa31e3158560c03fe3eb20cd14d9
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-02-16 12:28:01 +0530

    admin login signup

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -4,6 +4,7 @@ const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Teacher = require('../Schemas/Teacher');
+const Admin = require('../Schemas/Admin');
 
 
 exports.sign_up = [
@@ -53,6 +54,153 @@ exports.sign_up = [
     })
 
 ]
+
+
+
+exports.admin_sign_up = [
+    body('username').trim().escape().custom(userid => {
+        return new Promise((resolve, reject) => {
+            Student.findOne({ studentid: userid })
+                .then(idexists => {
+                    if (idexists) {
+                        reject(new Error("Id Already Exists"))
+                    } else {
+                        resolve(true)
+                    }
+                })
+        })
+    }).withMessage("username already exists"),
+    body('password').isLength({ min: 6 }).withMessage("Password should be at least 6 characters long"),
+    body('confirmpassword').custom((value, { req }) => {
+        return value === req.body.password
+    }).withMessage("confirm password doesn't match"),
+
+    asyncHandler(async function (req, res, next) {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+            bcrypt.hash(req.body.password, 10, async (err, hash) => {
+                const admin = new Admin({
+                    username: req.body.username,
+                    password: hash
+                })
+
+                await admin.save();
+                res.status(200).json({ status: "success" });
+            })
+        } else {
+            const admin = new Admin({
+                username:req.body.username
+            })
+
+            res.status(200).json({
+                errors: errors.array(),
+                student,
+            })
+        }
+    })
+
+]
+
+
+exports.admin_login = [
+    body('username').trim().escape(),
+    body('password'),
+
+
+    asyncHandler(async function (req, res, next) {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+
+            try {
+                const admin = await Admin.findOne({ username: req.body.username });
+
+                if (!admin) {
+                    res.status(200).json({
+                        errors: "Couldn't find a student with that ID",
+                    })
+                }
+
+                const match = await bcrypt.compare(req.body.password, admin.password);
+
+                if (!match) {
+                    res.status(200).json({
+                        errors: "Password was incorrect"
+                    })
+                }
+
+                const token = jwt.sign({username:admin.username, id:admin._id, role:admin.role} , process.env.SECRET, {
+                    expiresIn: 1000 * 60 * 60
+                });
+                res.status(200).json({
+                    token,
+                    status:"success"
+                })
+            } catch (err) {
+                console.log(err);
+                next(err)
+            }
+
+        } else {
+            res.status(200).json({
+                errors: errors.array(),
+                username: req.body.username
+            })
+
+        }
+    })
+]
+
+
+exports.login = [
+    body('studentid').trim().escape(),
+    body('password'),
+
+
+    asyncHandler(async function (req, res, next) {
+        const errors = validationResult(req);
+
+        if (errors.isEmpty()) {
+
+            try {
+                const student = await Student.findOne({ studentid: req.body.studentid });
+
+                if (!student) {
+                    res.status(200).json({
+                        errors: "Couldn't find a student with that ID",
+                    })
+                }
+
+                const match = await bcrypt.compare(req.body.password, student.password);
+
+                if (!match) {
+                    res.status(200).json({
+                        errors: "Password was incorrect"
+                    })
+                }
+
+                const token = jwt.sign({name:student.name, studentid:student.studentid , id:student._id, role:student.role} , process.env.SECRET, {
+                    expiresIn: 1000 * 60 * 60
+                });
+                res.status(200).json({
+                    token,
+                    status:"success"
+                })
+            } catch (err) {
+                console.log(err);
+                next(err)
+            }
+
+        } else {
+            res.status(200).json({
+                errors: errors.array(),
+                studentid: req.body.studentid
+            })
+
+        }
+    })
+] 
 
 
 exports.login = [
---
+++ Schemas/Admin.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+    username: String,
+    password, String,
+    role:{type:String, default:'admin'}
+})
+
+
+module.exports = mongoose.model("Admin", AdminSchema);
