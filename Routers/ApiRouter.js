a9e97538c9d25905878609d5f215e29f4eca6c4d
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-02-16 16:12:51 +0530

    sadf

--- Routers/ApiRouter.js
+++ Routers/ApiRouter.js
@@ -18,6 +18,7 @@ router.post('/addstudtoclass', APIController.addstud_to_class)
 router.get('/student/:studid', APIController.verify_student, APIController.get_stud)
 router.get("/lecture/:lectureid", APIController.verify_teacher, APIController.get_lecture)
 router.post('/admin/signup', authController.admin_sign_up);
+router.post('/admin/login', authController.admin_login)
 // router.post('/admin/login', authController.admin_login);
 // router.get('/get_classes', authController.verify_admin, APIController.get_classes)
 // router.get("/get_teachers", authController.verify_admin, APIController.get_teachers)
